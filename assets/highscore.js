0d8dee8e279202bd45987f541230591f6af5018c
Anjali Sheth <anjali.sheth@otis.com>
2020-12-15 17:59:51 -0500

    Added comments and formatting

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,32 +1,37 @@
+/* Selcting HTML elements to modify */
 hsEl = document.querySelector("#highscore");
-gobackEl = document.querySelector("#goback"); 
+gobackEl = document.querySelector("#goback");
 clearEl = document.querySelector("#clear");
 
+/* Function to navigate back to the main page */
 gobackEl.addEventListener("click", function () {
     console.log("Go back clicked");
     window.location.replace("../index.html");
-    }); 
+});
 
+/* Function to clear high scores */
 clearEl.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
-    }); 
+});
 
-var msg = localStorage.getItem("msg"); 
+/* Display time-up aleart when appropriate */
+var msg = localStorage.getItem("msg");
 var timeUpflag = localStorage.getItem('flag');
 if (msg !== "" && timeUpflag) {
-    console.log("TYpe of msg: ", typeof(msg), "msgg : ", msg);
+    console.log("TYpe of msg: ", typeof (msg), "msgg : ", msg);
     alert(msg);
 }
-var scoreList = localStorage.getItem("keepScore"); 
+var scoreList = localStorage.getItem("keepScore");
 scoreList = JSON.parse(scoreList);
 
+/* Display high scores if available in local storage */
 if (scoreList !== null) {
-for (i=0; i<scoreList.length; i++) {
-   console.log("High Score Array", scoreList[i].name, "Score", scoreList[i].score);
-   var scoreli = document.createElement("li");
-   scoreli.textContent =  (scoreList[i].name + ":                " + scoreList[i].score);
-   hsEl.appendChild(scoreli); 
-   }
+    for (i = 0; i < scoreList.length; i++) {
+        console.log("High Score Array", scoreList[i].name, "Score", scoreList[i].score);
+        var scoreli = document.createElement("li");
+        scoreli.textContent = (scoreList[i].name + ":                " + scoreList[i].score);
+        hsEl.appendChild(scoreli);
+    }
 }
 
--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,43 @@
+/* Defining Question Bank */
 var qBank = [
     {
-    question: "HTML stands for: ",
-    choices: ["Home Tool Madeup Language", "Hyperlinks Text Makeup Language", "Hyper Text Markup Language", "High Tech Markup Language"],
-    answer: "Hyper Text Markup Language",
+        question: "HTML stands for: ",
+        choices: ["Home Tool Madeup Language", "Hyperlinks Text Makeup Language", "Hyper Text Markup Language", "High Tech Markup Language"],
+        answer: "Hyper Text Markup Language",
     },
 
     {
-    question: "Which of the following is NOT a javascript datatype ",
-    choices: ["string", "number", "table", "boolean"],
-    answer: "table",
+        question: "Which of the following is NOT a javascript datatype ",
+        choices: ["string", "number", "table", "boolean"],
+        answer: "table",
     },
 
     {
-    question: "Which of the following is a component of CSS style rule?",
-    choices: ["Selector", "Property", "Value", "All of the above"],
-    answer: "All of the above",
+        question: "Which of the following is a component of CSS style rule?",
+        choices: ["Selector", "Property", "Value", "All of the above"],
+        answer: "All of the above",
     },
 
     {
-    question: "Which method is used to create a new HTML element through javascript?",
-    choices: ["createElement", "elementCreate", "addNewelement", "newElementadd"],
-    answer: "createElement",
+        question: "Which method is used to create a new HTML element through javascript?",
+        choices: ["createElement", "elementCreate", "addNewelement", "newElementadd"],
+        answer: "createElement",
     },
 
     {
-    question: "Javascript Math.random() returns: ",
-    choices: ["random number between 0 and infinity", "random number between 0 and 1", "whole numbers", "None of the above"],
-    answer: "random number between 0 and 1",     
+        question: "Javascript Math.random() returns: ",
+        choices: ["random number between 0 and infinity", "random number between 0 and 1", "whole numbers", "None of the above"],
+        answer: "random number between 0 and 1",
     }
- ]  
+]
 
- /* Selecting html elements */
- var timeleft = document.querySelector("#timeleft");
- var startquiz = document.querySelector("#startquiz");
- var quizSec = document.querySelector(".quiz-section")
- var container = document.querySelector(".container");
- var response = document.querySelector(".validate"); 
- var timeUp = document.querySelector("#Time-up");
+/* Selecting html elements */
+var timeleft = document.querySelector("#timeleft");
+var startquiz = document.querySelector("#startquiz");
+var quizSec = document.querySelector(".quiz-section")
+var container = document.querySelector(".container");
+var response = document.querySelector(".validate");
+var timeUp = document.querySelector("#Time-up");
 
 /* Defining gl0bal variables */
 var qIndex = 0;
@@ -44,19 +45,21 @@ var score = 0;
 var timer = 75;
 var holdInterval = 0;
 var penalty = 10;
-var scoreList = []; 
+var scoreList = [];
 var setInt;
-var timeUpflag = false; 
+var timeUpflag = false;
 
-startquiz.addEventListener('click', function(event) {
+startquiz.addEventListener('click', function (event) {
     event.preventDefault();
     setInt = setInterval(interval, 1000);
-        
-            presentquestions(qIndex);
+
+    presentquestions(qIndex);
 });
 localStorage.removeItem('flag');
-function interval (){
-    
+
+/* Funtion to check the timer */
+function interval() {
+
     timer--;
     timeleft.textContent = "Time left:  " + timer + "s";
     if (timer <= 0) {
@@ -67,129 +70,134 @@ function interval (){
         localStorage.setItem('msg', msg);
         clearInterval(setInt);
         window.location.replace("./assets/highscores.html");
-        
+
     }
 }
+/*  Function to present questions and multiple choice answsers to user */
 function presentquestions(i) {
     if (localStorage.getItem('msg') !== null) {
         localStorage.removeItem('msg');
         console.log("local storage msg removed");
     }
     quizSec.innerHTML = "";
-        
+
     var userQ = qBank[i].question;
     var userC = qBank[i].choices;
-        
-    console.log("Question is: " , userQ);
+
+    console.log("Question is: ", userQ);
     quizSec.textContent = userQ;
-                        
-    var ul = document.createElement("ul"); 
-    quizSec.appendChild(ul); 
-    for (j=0; j<userC.length; j++) {
+
+    var ul = document.createElement("ul");
+    quizSec.appendChild(ul);
+    for (j = 0; j < userC.length; j++) {
         var buttonEl = document.createElement('BUTTON');
         var linebreak = document.createElement('br')
         ul.appendChild(linebreak);
         ul.appendChild(buttonEl);
-            
+
         buttonEl.textContent = userC[j];
         buttonEl.setAttribute('id', j);
         buttonEl.setAttribute('class', "ansbtn");
     }
-    var btnList = document.querySelector("ul"); 
+    var btnList = document.querySelector("ul");
     console.log("btnEl Created ", btnList);
-    btnList.addEventListener('click', function(event) {
-    console.log(event); 
-    event.preventDefault();
-    if(event.target.matches("button")) {
-        console.log(event.target.id); 
-        var index = parseInt(event.target.id); 
-        
-        console.log("Correct Answer: ", qBank[i].answer);
-        console.log("User Answer: ", userC[index]);
+    btnList.addEventListener('click', function (event) {
+        console.log(event);
+        event.preventDefault();
+        if (event.target.matches("button")) {
+            console.log(event.target.id);
+            var index = parseInt(event.target.id);
 
-        if((userC[index]) === qBank[i].answer) {
-            console.log("Inside if loop");
-            response.textContent = "Correct Answer";
-            score++;
-        }
-        else {
-            console.log("Inside else loop");
-            response.innerHTML = "Wrong Answer";
-            timer = timer -15;
+            console.log("Correct Answer: ", qBank[i].answer);
+            console.log("User Answer: ", userC[index]);
+
+            if ((userC[index]) === qBank[i].answer) {
+                console.log("Inside if loop");
+                response.textContent = "Correct Answer";
+                score++;
+            }
+            else {
+                console.log("Inside else loop");
+                response.innerHTML = "Wrong Answer";
+                timer = timer - 15;
             }
             qIndex++;
             console.log("Index is: ", qIndex);
-            if (qIndex<5) {
+            if (qIndex < 5) {
                 presentquestions(qIndex);
             }
             else {
                 console.log("Questions Correct: ", score);
                 alldone(timer);
-               }
-             }
-            });
+            }
+        }
+    });
 
-function alldone (timer) {
-    console.log("score passed to alldone: ", timer);
-   /*  clearInterval(setInt);
-     */
-    quizSec.innerHTML = "";
-    response.innerHTML = ""; 
-    var h4El = document.createElement("h4");``
-    quizSec.appendChild(h4El);
-    h4El.textContent = "All Done!!";
-    
-    var pEl = document.createElement("p");
-    quizSec.appendChild(pEl);
-    pEl.textContent = "Your Score is " + timer;
-     
+    /*  Function to dynamically create the page so user can submit high scores */
+    function alldone(timer) {
+        console.log("score passed to alldone: ", timer);
+        /*  clearInterval(setInt);
+          */
+        quizSec.innerHTML = "";
+        response.innerHTML = "";
+        var h4El = document.createElement("h4"); ``
+        quizSec.appendChild(h4El);
+        h4El.textContent = "All Done!!";
 
-    var labelEl = document.createElement("label");
-    labelEl.setAttribute("id", "createLabel");
-    labelEl.textContent = "Enter your initials: ";
+        var pEl = document.createElement("p");
+        quizSec.appendChild(pEl);
+        pEl.textContent = "Your Score is " + timer;
 
-    quizSec.appendChild(labelEl);
 
-    // input
-    var InputEl = document.createElement("input");
-    InputEl.setAttribute("type", "text");
-    InputEl.setAttribute("id", "initials");
-    InputEl.textContent = "";
+        var labelEl = document.createElement("label");
+        labelEl.setAttribute("id", "createLabel");
+        labelEl.textContent = "Enter your initials: ";
 
-    quizSec.appendChild(InputEl);
+        quizSec.appendChild(labelEl);
 
-    // submit
-    var SubmitEl = document.createElement("button");
-    SubmitEl.setAttribute("type", "submit");
-    SubmitEl.setAttribute("id", "Submit");
-    SubmitEl.textContent = "Submit";
+        // input
+        var InputEl = document.createElement("input");
+        InputEl.setAttribute("type", "text");
+        InputEl.setAttribute("id", "initials");
+        InputEl.textContent = "";
 
-    quizSec.appendChild(SubmitEl);
+        quizSec.appendChild(InputEl);
 
-    SubmitEl.addEventListener('click', function() {
+        // submit
+        var SubmitEl = document.createElement("button");
+        SubmitEl.setAttribute("type", "submit");
+        SubmitEl.setAttribute("id", "Submit");
+        SubmitEl.textContent = "Submit";
 
-        var initials = InputEl.value;
-        var str;
-        
-        if (initials === "" ){
-            alert("Please enter initials to save scores");
-        } 
-        else{
-            console.log("type of initials: ", typeof(initials), initials);
-            if (localStorage.getItem("keepScore") === null){
-                scoreList.push( {name : initials, score : timer });
-                str = JSON.stringify(scoreList)
-                localStorage.setItem("keepScore", str); 
+        quizSec.appendChild(SubmitEl);
+
+        /*  Function to Submit high scores */
+
+        SubmitEl.addEventListener('click', function () {
+
+            var initials = InputEl.value;
+            var str;
+
+            if (initials === "") {
+                alert("Please enter initials to save scores");
             }
             else {
-                str = localStorage.getItem("keepScore"); 
-                str = JSON.parse(str);
-                str.push( {name : initials, score : timer });
-                scoreList = str;
-                str = JSON.stringify(str);
-                localStorage.setItem("keepScore",str);
-            }        
-            window.location.replace("./assets/highscores.html");          
-        }; 
-    }); 
-}}               
+                console.log("type of initials: ", typeof (initials), initials);
+                if (localStorage.getItem("keepScore") === null) {
+                    scoreList.push({ name: initials, score: timer });
+                    str = JSON.stringify(scoreList)
+                    localStorage.setItem("keepScore", str);
+                }
+                else {
+                    str = localStorage.getItem("keepScore");
+                    str = JSON.parse(str);
+                    str.push({ name: initials, score: timer });
+                    scoreList = str;
+                    str = JSON.stringify(str);
+                    localStorage.setItem("keepScore", str);
+                }
+                window.location.replace("./assets/highscores.html");
+            };
+        });
+    }
+}               
